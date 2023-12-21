020a8356e9af9060fa9fd76100d5a30b40bcc284
马逸譞 <mayixuan@udesk.cn>
2023-12-21 09:57:36 +0800

    feat: add umi-like commander

--- packages/framework/bin/umi-like.js
+++ packages/framework/bin/umi-like.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 
-console.log('hello umi-like');
+const { program } = require("commander");
+const { version } = require("../package.json");
+
+program
+    .version(version, "-v, --version", "输出框架当前版本")
+    .description("类Uim框架")
+    .usage("<command> [options]")
+    .parse(process.argv);
+
+program.command('start')
+    .description('启动服务')
+    .action(function (name, other) {
+        console.log(`==== run start ====`)
+    }).parse(process.argv);
