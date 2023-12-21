e931c0256a039ea4c9965410f6b0b2bddfd961fe
马逸譞 <mayixuan@udesk.cn>
2023-12-21 11:24:15 +0800

    feat: 完善examples app，先使用esbuild跑通流程

---
+++ examples/app/src/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const Hello = () => {
+  const [text, setText] = React.useState("Hello Umi-like!");
+  return (
+    <span
+      onClick={() => {
+        setText("Hi!");
+      }}
+    >
+      {text}
+    </span>
+  );
+};
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(React.createElement(Hello));
--- packages/framework/bin/umi-like.js
+++ packages/framework/bin/umi-like.js
@@ -6,11 +6,13 @@ const { version } = require("../package.json");
 program
     .version(version, "-v, --version", "输出框架当前版本")
     .description("类Uim框架")
-    .usage("<command> [options]")
-    .parse(process.argv);
+    .usage("<command> [options]");
 
-program.command('start')
-    .description('启动服务')
+program.command('dev')
+    .description('框架启动服务')
     .action(function (name, other) {
-        console.log(`==== run start ====`)
-    }).parse(process.argv);
+        console.log(`==== run dev ====`)
+        require('../lib/dev');
+    });
+
+program.parse(process.argv);
