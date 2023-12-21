41a5c3c76de85424be274508f3474726726f07a4
马逸譞 <mayixuan@udesk.cn>
2023-12-21 14:26:41 +0800

    feat: 使用Umi-like dev serve返回html入口

---
+++ packages/framework/src/constants.ts
@@ -0,0 +1,17 @@
+export const DEFAULT_PORT = 8888;
+
+export const htmlTemplate = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Umi-like</title>
+  </head>
+
+  <body>
+    <div id="root">
+      <span>loading...</span>
+    </div>
+    <script src="./index.js"></script>
+  </body>
+</html>
+`;
--- packages/framework/src/dev.ts
+++ packages/framework/src/dev.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import { DEFAULT_PORT, htmlTemplate } from "./constants";
 
 export const dev = async () => {
   const app = express();
 
-  app.listen(8888, async () => {
-    console.log(`App listening at http://localhost:8888`);
+  app.get("/", (_, res) => {
+    // see https://expressjs.com/en/api.html#res.set
+    res.set("Content-Type", "text/html");
+    res.send(htmlTemplate);
+  });
+
+  app.listen(DEFAULT_PORT, async () => {
+    console.log(`Umi-like start at http://localhost:8888`);
   });
 };
